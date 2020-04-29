b9ec875d277638de0b30601708e82472b89bdba5
Doug Binder <douglasjbinder@gmail.com>
2020-04-29 13:00:51 -0700

    fix

--- launch.sh
+++ launch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 rm /etc/nginx/conf.d/default.conf || :
-envsubst < nginx.conf > /etc/nginx/conf.d/nginx.conf
+envsubst < nginx.conf > /etc/nginx/nginx.conf
 envsubst < auth.conf > /etc/nginx/conf.d/auth.conf
 envsubst < auth.htpasswd > /etc/nginx/auth.htpasswd
 
