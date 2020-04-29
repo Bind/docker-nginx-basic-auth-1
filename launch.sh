217f388c9ce1830a02a7b95e2772c6d7ccd408ce
Doug Binder <douglasjbinder@gmail.com>
2020-04-29 13:53:06 -0700

    envsubst

--- launch.sh
+++ launch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 rm /etc/nginx/conf.d/default.conf || :
-envsubst < auth.conf > /etc/nginx/conf.d/auth.conf
-envsubst < 00-websockets.conf > /etc/nginx/conf.d/00-websockets.conf
+envsubst '${FORWARD_HOST},${FORWARD_PORT}' < auth.conf > /etc/nginx/conf.d/auth.conf
+envsubst '${FORWARD_HOST},${FORWARD_PORT}' < 00-websockets.conf > /etc/nginx/conf.d/00-websockets.conf
 envsubst < auth.htpasswd > /etc/nginx/auth.htpasswd
 
 nginx -g "daemon off;"
