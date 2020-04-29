b55656b25d8fee8c1842110bf5fd3d746fa8f8a5
Doug Binder <douglasjbinder@gmail.com>
2020-04-29 13:30:44 -0700

    websocket conf

--- launch.sh
+++ launch.sh
@@ -2,6 +2,7 @@
 
 rm /etc/nginx/conf.d/default.conf || :
 envsubst < auth.conf > /etc/nginx/conf.d/auth.conf
+envsubst < 00-websockets.conf > /etc/nginx/conf.d/00-websockets.conf
 envsubst < auth.htpasswd > /etc/nginx/auth.htpasswd
 
 nginx -g "daemon off;"
